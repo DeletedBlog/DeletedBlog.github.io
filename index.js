b09f31d584cf81129a6572391cd21afbb6a1e6da
DeletedBlog <tristanfarrington@gmail.com>
2017-06-15 13:37:09 -0400

    Added interactive stuff

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-
+$(document).ready(function(){
+  $('.toplink').mouseenter(function(){
+    $('.toplinktext').fadeTo('fast', 0.5);
+  });
+  $('.toplink').mouseleave(function(){
+    $('.toplinktext').fadeTo('fast',1);
+  });
+});
