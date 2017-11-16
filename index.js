f2dc080387d9aa48646dc6be9052c593dfa99d6d
chatsup <tristanfarrington@gmail.com>
2017-11-16 13:00:37 -0500

    Update index.js

--- index.js
+++ index.js
@@ -10,16 +10,27 @@ app.get('/', function(req, res){
 io.on('connection', function(socket){
   socket.on('join', function(room){
     if (socket.room != undefined) {
+      if (socket.username != undefined) {
+        io.sockets.in(socket.room).emit('chat message', socket.username + "has joined your channel");
+      } else {
+        io.sockets.in(socket.room).emit('chat message', "anonymous has joined your channel")
+      }
       socket.leave(socket.room);
     }
     socket.join(room);
     socket.room = room;
+    if (socket.username != undefined) {
+      io.sockets.in(socket.room).emit('chat message', socket.username + "has joined your channel");
+    } else {
+      io.sockets.in(socket.room).emit('chat message', "anonymous has joined your channel")
+    }
   });
   socket.on('chat message', function(msg){
     io.sockets.in(socket.room).emit('chat message', msg);
   });
   socket.on('username', function(usnm){
-    io.sockets.in(socket.room).emit('username', usnm);
+    socket.username = usnm;
+    io.sockets.in(socket.room).emit('username', socket.username);
   });
 });
 
