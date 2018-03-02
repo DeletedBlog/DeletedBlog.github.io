8a2fd0c66a49282a9a5f5783e08db7a93db9508d
retardedboi <tristanfarrington@gmail.com>
2018-03-02 15:56:20 -0500

    Update index.js

--- index.js
+++ index.js
@@ -7,8 +7,6 @@ app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
-usedIds = [];
-
 io.on('connection', function(socket){
   socket.on('join', function(room){
     if (socket.room != undefined) {
@@ -21,9 +19,6 @@ io.on('connection', function(socket){
   socket.on('chat message', function(msg){
     io.sockets.in(socket.room).emit('chat message', msg);
   });
-  socket.on('message id', function(msgid){
-    io.sockets.in(socket.room).emit('message id', msgid);
-  };
   socket.on('username', function(usnm){
     io.sockets.in(socket.room).emit('username', usnm);
     socket.username = usnm;
